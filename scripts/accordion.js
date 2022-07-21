f594c0e2becdd3172af6982d103fc578a2ba54a4
Nurik4k <56000114+Nurik4k@users.noreply.github.com>
2022-07-21 23:44:39 +0600

    Add files via upload
    change id to class

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,7 +1,7 @@
 
 function dropdown(event) {
    if (event.target.className != "accordion__lable-title"
-      && event.target.id != "dropdown"
+      && event.target.className != "dropdown"
       && event.target.className != "accordion__label") return;
    let item = event.target.closest(".accordion__item-inner")
    item.classList.toggle('active');
